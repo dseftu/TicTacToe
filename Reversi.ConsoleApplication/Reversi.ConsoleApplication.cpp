932965e8e27baa784f233224ecb03f1d5f0fbf31
David M Allen <dseftu@gmail.com>
2017-11-20 13:39:45 -0500

    initial project setup

---
+++ Reversi.ConsoleApplication/Reversi.ConsoleApplication.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+using namespace std;
+using namespace Library;
+
+// the foundation for which this house will be built
+int main(int argc, char* argv[])
+{
+	UNREFERENCED_PARAMETER(argc);
+	UNREFERENCED_PARAMETER(argv);
+
+	return 0;
+}
